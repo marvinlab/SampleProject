1dfe971bc17068f970d15ff1f019549531ee1ac3
Temp User <tempuser@MAC112.local>
2015-02-05 12:04:54 +0800

    Initial Commit

---
+++ SampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleProject
+//
+//  Created by Temp User on 2/5/15.
+//  Copyright (c) 2015 Temp User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by Temp User on 2/5/15.
+//  Copyright (c) 2015 Temp User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
